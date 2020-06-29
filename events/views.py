b7a36c599bd399407e56747c1604ecdf6b4825b7
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-06-29 15:43:07 +0530

    Starting the deployment on heroku

--- events/views.py
+++ events/views.py
@@ -10,17 +10,13 @@ from rest_framework import status
 class Events(APIView):
     def post(self, request, *args, **kwargs):
         text = request.data.get('text', None)
-        SLACK_BOT_USER_TOKEN = request.data.get('token')
-        SLACK_CHANNEL = request.data.get('channel')
-        SLACK_ICON_URL = request.data.get('icon_url')
-        SLACK_USER_NAME = request.data.get('username')
 
         resp = requests.post('https://slack.com/api/chat.postMessage', {
-            'token': json.dumps(SLACK_BOT_USER_TOKEN),
-            'channel': json.dumps(SLACK_CHANNEL),
+            'token': "xoxb-1234958449984-1211347637395-EX8IKUZ9EmewbbYRXIPfLed3",
+            'channel': "#website",
             'text': text,
-            'icon_url': json.dumps(SLACK_ICON_URL),
-            'username': json.dumps(SLACK_USER_NAME)
+            'icon_url': "https://mycocosoul.s3.amazonaws.com/static/img/logo.png",
+            'username': "easyeats"
         }).json()
 
         return Response(resp)
